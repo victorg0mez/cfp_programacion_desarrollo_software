6bcf3397e9dc9e7f391ffdfe908727473f9d0dfa
victorg0mez <victorgomez@CFPBARTOLOMEAMBROSIO.COM>
2024-07-10 16:00:49 -0600

    Cambios subidos

---
+++ Modulo_2/Node/calculate.js
@@ -0,0 +1,23 @@
+module.exports = {suma,resta,multiplicar,dividir}
+
+
+function suma(a,b) {
+    suma = a + b
+    return suma 
+};
+
+function resta(a,b) {
+    resta = a - b
+    return resta 
+};
+
+function multiplicar(a,b) {
+    multiplicar = a * b
+    return multiplicar 
+};
+
+function dividir(a,b) {
+    dividir = a / b
+    return dividir 
+};
+
---
+++ Modulo_2/Node/estudiantes.js
@@ -0,0 +1,6 @@
+let {suma:funSum,resta:funRes,multiplicar:funMul,dividir:funDiv} = require('./calculate.js');
+console.log(funSum(5,100));
+console.log(funRes(5,1));
+console.log(funMul(12,11));
+console.log(funDiv(10,20));
+
